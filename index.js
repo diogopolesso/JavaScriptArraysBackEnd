9ebb2ae201607db7e29a2b8fd4da6b86a908e5e5
diogopolesso <diogopolesso@gmail.com>
2022-11-09 16:32:09 -0300

    Jantar de Hoje

---
+++ index.js
@@ -0,0 +1,10 @@
+jantarDeHoje = ['burguer', 'hotdog', 'pizza']
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('ovo')
+jantarDeHoje.push('salada')
+jantarDeHoje.push('maça')
+
+console.log(jantarDeHoje)
