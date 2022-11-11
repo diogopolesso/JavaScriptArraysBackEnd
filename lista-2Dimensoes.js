44f9ea7bfecafe5a8bd42367661ed30c2bd4c717
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-11-11 18:25:02 -0300

    Update lista-2Dimensoes.js

--- lista-2Dimensoes.js
+++ lista-2Dimensoes.js
@@ -1,3 +1,6 @@
+/* Listas com duas dimensões, ou seja, arrays que contêm arrays, podendo ter um único array ou vários dentro do principal.*/
+
+
 const alunos = ['João', 'Juliana', 'Caio'];
 const medias = [10, 8, 7.5, 9];
 
